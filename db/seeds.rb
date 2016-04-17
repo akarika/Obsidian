7938667d76356797507947e63066d0a9ce6760fa
Akarika <roux.remy.m@gmail.com>
2016-04-17 20:56:18 +0200

    ajout gem faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+include Faker
+5.times do |_n|
+  email = Faker::Internet.email
+  password = 'password'
+  User.create!(email: email,
+               password:              password,
+               password_confirmation: password)
+end
+7.times do |_n|
+  title = Faker::Lorem.sentence(3, false, 4)
+  content = Faker::Lorem.paragraph(4, false, 6)
+  Article.create!(title: title,
+                  content: content)
+end
+Admin.create!(email: 'admin@ad.com',
+              password: 'admin_admin',
+              password_confirmation: 'admin_admin')
+User.create!(email: 'user@u.com',
+             password:              'user_example',
+             password_confirmation: 'user_example')
